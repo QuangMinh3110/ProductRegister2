55acf401a08709799ffb9cd55c40149009a4e206
QuangMinh3110 <55874218+QuangMinh3110@users.noreply.github.com>
2019-10-09 15:50:25 +0700

    qqq

---
+++ ProductRegist.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Processing</title>
+</head>
+<body>
+	<?php
+		$productid = $_POST["txtid"];	 
+		$name = $_POST["txtName"];
+		$price = $_POST["txtprice"];	
+		echo $name;
+		
+		//Refere to database 
+	   $db = parse_url(getenv("DATABASE_URL"));
+	   $pdo = new PDO("pgsql:" . sprintf(
+	        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	        $db["host"],
+	        $db["port"],
+	        $db["user"],
+	        $db["pass"],
+	        ltrim($db["path"], "/")
+	   ));
+	   $data = [
+		    'productid' => $producid,
+		    'name' => $name,
+		    'price' => $price,
+		];
+		$stmt =  $pdo->prepare("INSERT INTO product (productid, name, price) VALUES (:productid,:name,:price)");	
+		$stmt->execute($data);
+	 ?>
+	 <h2>Thank you <?php echo $name?>  for registering 
+	 		<?php echo $name?>
+	 </h2>
+	 <ul>
+	 	<li><?php echo $productid?></li>
+	 	<li><?php echo $name?></li>
+	 	<li><?php echo $prict?></li>
+	 </ul>
+	 <a href="index2.php">Index2</a>
+</body>
+</html>
